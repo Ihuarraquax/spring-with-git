9771c14f3296e4ef7602f446de909eca028a448d
Ihuarraquax <hzablocki97@gmail.com>
2019-12-15 10:32:58 +0100

    [add] - UserController impl

--- src/main/java/pl/zablocki/springwithgit/controllers/UserController.java
+++ src/main/java/pl/zablocki/springwithgit/controllers/UserController.java
@@ -1,9 +1,14 @@
 package pl.zablocki.springwithgit.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
 
 }
