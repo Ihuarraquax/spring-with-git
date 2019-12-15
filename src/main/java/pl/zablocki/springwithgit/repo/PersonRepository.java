6dc30e9ad0bd41cbf24e1ec11175ff9307779ba3
Ihuarraquax <hzablocki97@gmail.com>
2019-12-15 12:32:41 +0100

    [add] - find all and order method

--- src/main/java/pl/zablocki/springwithgit/repo/PersonRepository.java
+++ src/main/java/pl/zablocki/springwithgit/repo/PersonRepository.java
@@ -3,5 +3,8 @@ package pl.zablocki.springwithgit.repo;
 import org.springframework.data.repository.CrudRepository;
 import pl.zablocki.springwithgit.domain.Person;
 
+import java.util.List;
+
 public interface PersonRepository extends CrudRepository<Person, Long> {
+    List<Person> findAllOrderBySurname();
 }
